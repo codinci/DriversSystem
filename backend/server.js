f664ecde341cd35fb1b960af3c392fb1b5684698
codinci <dkibau@gmail.com>
2023-02-14 10:53:47 +0300

    user registration and login

--- backend/server.js
+++ backend/server.js
@@ -4,24 +4,16 @@ const path = require('path');
 const { logger } = require('./middleware/logEvents');
 const errorHandler = require('./middleware/errorHandler');
 const cors = require('cors');
+const corsOptions = require('./config/corsOptions');
 
 const PORT = process.env.PORT || 3500;
 
 //custom middleware logger
 app.use(logger);
+
 //Cross Origin Resource Sharing
-const whiteList = ['https://www.mySite.com', 'http://127.0.0.1:5500', 'http://localhost:3500']
-const corsOptions = {
-	origin: (origin, callback) => {
-		if (whiteList.indexOf(origin) !== -1 || !origin) {
-			callback(null, true)
-		} else {
-			callback(new Error('Not allowed by CORS'));
-		}
-	},
-	optionsSuccessStatus: 200
-}
 app.use(cors(corsOptions));
+
 //middleware to handle form data
 app.use(express.urlencoded({ extended: false }));
 
@@ -32,9 +24,11 @@ app.use(express.json());
 app.use(express.static(path.join(__dirname, '/public')));
 app.use('/subdir', express.static(path.join(__dirname, '/public')));
 
-app.use('/subdir', require('./routes/subdir'));
 app.use('/', require('./routes/root'));
+app.use('/subdir', require('./routes/subdir'));
 app.use('/employees', require('./routes/api/employees'));
+app.use('/register', require('./routes/register'));
+app.use('/auth', require('./routes/auth'));
 
 // route handlers
 app.get('/hello(.html)?', (req, res, next) => {
