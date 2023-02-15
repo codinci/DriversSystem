dad0bdc1a846d46eab542b52cc4f145d930367c7
codinci <dkibau@gmail.com>
2023-02-15 07:59:55 +0300

    added jwt authentication and cookie for refresh token

--- backend/server.js
+++ backend/server.js
@@ -5,11 +5,17 @@ const { logger } = require('./middleware/logEvents');
 const errorHandler = require('./middleware/errorHandler');
 const cors = require('cors');
 const corsOptions = require('./config/corsOptions');
+const verifyJWT = require('./middleware/verifyJWT');
+const cookieParser = require('cookie-parser');
+const credentials = require('./middleware/credentials');
 
 const PORT = process.env.PORT || 3500;
 
 //custom middleware logger
 app.use(logger);
+
+//assign credentials to header or request
+app.use(credentials);
 
 //Cross Origin Resource Sharing
 app.use(cors(corsOptions));
@@ -20,15 +26,24 @@ app.use(express.urlencoded({ extended: false }));
 //middleware for json
 app.use(express.json());
 
+//middleware for cookies
+app.use(cookieParser());
+
 //serve static files
 app.use(express.static(path.join(__dirname, '/public')));
 app.use('/subdir', express.static(path.join(__dirname, '/public')));
 
 app.use('/', require('./routes/root'));
 app.use('/subdir', require('./routes/subdir'));
-app.use('/employees', require('./routes/api/employees'));
 app.use('/register', require('./routes/register'));
 app.use('/auth', require('./routes/auth'));
+app.use('/refresh', require('./routes/refresh'));
+app.use('/logout', require('./routes/logout'));
+
+
+app.use(verifyJWT);
+app.use('/employees', require('./routes/api/employees'));
+
 
 // route handlers
 app.get('/hello(.html)?', (req, res, next) => {
